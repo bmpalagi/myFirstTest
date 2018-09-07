4ff9fd9efd7ac0c9015c578700c062270c5c9c9a
Brad Palagi <bmpalagi@stu.naperville203.org>
2018-09-07 12:18:56 -0500

    Started Parking

---
+++ again another Test/parking.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class parking
+{
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Julies Parking, enter your license plate number to add your car");
+        System.out.println("put a negetive infront of the licenseplate to remove it, put a 0 to end");
+        
+        int decision;
+        Scanner scan = new Scanner(System.in);
+        
+        
+    }
+}
