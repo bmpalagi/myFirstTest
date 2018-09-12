09f4207cb65315947a1c8e7d5de2554b3dfcee49
Brad Palagi <bmpalagi@stu.naperville203.org>
2018-09-12 13:00:43 -0500

    Started Flood Fill

---
+++ again another Test/pair.java
@@ -0,0 +1,19 @@
+
+public class pair
+{
+    
+    int row, collumn, value;
+    
+    public pair(int x, int y)
+    {
+        row = x;
+        collumn = y;
+        value = 0;
+        
+    }
+    
+    public void changeValue(int v)
+    {
+        value = v;
+    }
+}
