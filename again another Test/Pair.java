86865fb3dca1b03aefd51075faa35113aaffb6db
Brad Palagi <bmpalagi@stu.naperville203.org>
2018-09-13 12:22:28 -0500

    Working on flood fill

---
+++ again another Test/Pair.java
@@ -0,0 +1,29 @@
+
+public class Pair
+{
+    
+    int row, collumn, value;
+    
+    public Pair(int x, int y, int v)
+    {
+        row = x;
+        collumn = y;
+        value = v;
+        
+    }
+    
+    public int getRow()
+    {
+        return row;
+    }
+    
+    public int getCollumn()
+    {
+        return collumn;
+    }
+    
+    public int getValue()
+    {
+        return value;
+    }
+}
